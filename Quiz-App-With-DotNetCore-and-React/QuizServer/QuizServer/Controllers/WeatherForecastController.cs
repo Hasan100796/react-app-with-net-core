ea53de162b5d5dee2a1c8c9227f0f5375114bc93
Hasan <hasana773@gmail.com>
2022-08-21 01:10:19 +0530

    modified the default controller name

--- Quiz-App-With-DotNetCore-and-React/QuizServer/QuizServer/Controllers/WeatherForecastController.cs
+++ Quiz-App-With-DotNetCore-and-React/QuizServer/QuizServer/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace QuizServer.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[default]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
